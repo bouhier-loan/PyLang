7792fb48f4c85c670e49f2f5c64b39977222c4e0
Loan <35420635+Dobble85@users.noreply.github.com>
2023-02-06 02:02:36 +0100

    [CREATED] Project [TODO] Function name bug fix

---
+++ shell.py
@@ -0,0 +1,9 @@
+import PyLang as pl
+
+while True:
+    text = input('PyLang > ')
+    if text == '': break
+    result, error = pl.run('<stdin>', text)
+
+    if error: print(error)
+    elif result: print(result)
